21c577cbf872ea270b9c33336c45ab9ab37404cd
Grant Garcia <github@grantgarcia.org>
2013-02-22 17:55:53 -0700

    Externalized database config

--- washboard/settings.py
+++ washboard/settings.py
@@ -153,3 +153,33 @@ LOGGING = {
         },
     }
 }
+
+# From http://djangosnippets.org/snippets/1873/
+try:
+    import local_settings
+except ImportError:
+    print """ 
+        -------------------------------------------------------------------------
+        You need to create a local_settings.py file which needs to contain at least
+        database connection information.
+        
+        Copy local_settings_example.py to local_settings.py and edit it.
+        -------------------------------------------------------------------------
+        """
+    import sys 
+    sys.exit(1)
+else:
+    # Import any symbols that begin with A-Z. Append to lists any symbols that
+    # begin with "EXTRA_".
+    import re
+    for attr in dir(local_settings):
+        match = re.search('^EXTRA_(\w+)', attr)
+        if match:
+            name = match.group(1)
+            value = getattr(local_settings, attr)
+            try:
+                globals()[name] += value
+            except KeyError:
+                globals()[name] = value
+        elif re.search('^[A-Z]', attr):
+            globals()[attr] = getattr(local_settings, attr)
