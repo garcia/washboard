9b6e345d1db6fded4abc172ba212854404f42959
Grant Garcia <github@grantgarcia.org>
2013-02-27 20:42:10 -0700

    Added shift-click functionality to default checkboxes

--- static/js/rules.js
+++ static/js/rules.js
@@ -13,16 +13,34 @@ function add_rule() {
     // Insert the new row into the rules table
     rule.insertBefore($('.rule.add'));
     rule.find('.keyword input').focus().keypress(keyword_keypress);
+    rule.find('input[type=checkbox]').click(checkbox_click);
 }
 
 function keyword_keypress(e) {
-    console.log('beep');
     if (e.which == 13) {
-        console.log('boop');
         add_rule();
+    }
+}
+
+function checkbox_click(e) {
+    console.log(e);
+    if (e.shiftKey) {
+        if ($(this).closest('.rule').hasClass('defaults')) {
+            $('.' + $(this).closest('div').attr('class'))
+                .find('input[type=checkbox]')
+                .prop('checked', this.checked);
+        }
+        /* I'm not sure if I like this idea or not...
+        else {
+            $(this).closest('.rule')
+                .find('input[type=checkbox]')
+                .prop('checked', this.checked);
+        }
+        */
     }
 }
 
 $(function() {
     $('.keyword input').keypress(keyword_keypress);
+    $('input[type=checkbox]').click(checkbox_click);
 });
