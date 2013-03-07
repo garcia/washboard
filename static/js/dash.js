e47d37ed764cd08ec3f3bd29d3cb0291d1b5fefb
Grant Garcia <github@grantgarcia.org>
2013-03-06 18:55:48 -0700

    Cleanup: $(elem(n)) -> elem(n)

--- static/js/dash.js
+++ static/js/dash.js
@@ -40,30 +40,30 @@ function cb(data) {
         if(typeof(elem) != "string") {
             return false;
         }
-        return document.createElement(elem);
+        return $(document.createElement(elem));
     }
     d = data;
-    posts = $(elem('ul')).attr('id', 'posts');
+    posts = elem('ul').attr('id', 'posts');
     $.each(data.response.posts, function(p, post) {
         console.log(post);
-        var postelem = $(elem('li')).addClass('post');
+        var postelem = elem('li').addClass('post');
         postelem.addClass(post.type);
 
         var scan = [];
 
         // Common metadata
-        var meta = $(elem('div')).addClass('meta');
-        meta.append($(elem('a'))
+        var meta = elem('div').addClass('meta');
+        meta.append(elem('a')
             .addClass('blog_name')
             .text(post.blog_name)
             .attr('href', post.post_url)
         );
         if (post.reblogged_from_name) {
-            meta.append($(elem('span'))
+            meta.append(elem('span')
                 .addClass('reblogged_text')
                 .html('&nbsp;reblogged&nbsp;')
             );
-            meta.append($(elem('a'))
+            meta.append(elem('a')
                 .addClass('reblogged_from_name')
                 .text(post.reblogged_from_name)
                 .attr('href', post.reblogged_from_url)
@@ -71,7 +71,7 @@ function cb(data) {
             scan.push(post.reblogged_from_name);
         }
         if (post.note_count) {
-            meta.append($(elem('a'))
+            meta.append(elem('a')
                 .addClass('note_count')
                 .text(post.note_count)
             );
@@ -82,17 +82,17 @@ function cb(data) {
         // Text
         if (post.type == 'text') {
             if (post.title) {
-                postelem.append($(elem('h2')).text(post.title));
+                postelem.append(elem('h2').text(post.title));
                 scan.push(post.title);
             }
-            postelem.append($(elem('div')).addClass('body').html(post.body));
+            postelem.append(elem('div').addClass('body').html(post.body));
             scan.push(post.body);
         }
 
         // Photo
         else if (post.type == 'photo') {
-            var photos = $(elem('div')).addClass('photos');
-            var row = $(elem('div')).addClass('row');
+            var photos = elem('div').addClass('photos');
+            var row = elem('div').addClass('row');
             var last_row = 0;
             $.each(post.photos, function(ph, photo) {
                 // Photoset logic
@@ -112,14 +112,14 @@ function cb(data) {
                 if (running_row > last_row) {
                     row.addClass('row-' + layout[last_row]);
                     photos.append(row);
-                    row = $(elem('div')).addClass('row');
+                    row = elem('div').addClass('row');
                 }
                 last_row = running_row;
                 var target_size = {'1': 500 * scale, '2': 245 * scale, '3': 160 * scale}[
                     layout[running_row]
                 ];
                 var best_photo = best_fit(photo.alt_sizes, target_size);
-                var photoelem = $(elem('img'))
+                var photoelem = elem('img')
                     .attr('src', best_photo.url);
                 row.append(photoelem);
             });
@@ -127,24 +127,24 @@ function cb(data) {
             photos.append(row);
             postelem.append(photos);
             if (post.caption) {
-                postelem.append($(elem('div')).addClass('caption').html(post.caption));
+                postelem.append(elem('div').addClass('caption').html(post.caption));
                 scan.push(post.caption);
             }
         }
 
         // Quote
         else if (post.type == 'quote') {
-            postelem.append($(elem('div')).addClass('quote').html(post.text));
+            postelem.append(elem('div').addClass('quote').html(post.text));
             scan.push(post.text);
             if (post.source) {
-                postelem.append($(elem('div')).addClass('source').html(post.source));
+                postelem.append(elem('div').addClass('source').html(post.source));
                 scan.push(post.source);
             }
         }
 
         // Link
         else if (post.type == 'link') {
-            var anchor = $(elem('a')).addClass('link').attr('href', post.url);
+            var anchor = elem('a').addClass('link').attr('href', post.url);
             if (post.title) {
                 anchor.text(post.title);
             }
@@ -155,7 +155,7 @@ function cb(data) {
             scan.push(post.url);
             postelem.append(anchor);
             if (post.description) {
-                postelem.append($(elem('div')).addClass('description').html(post.description));
+                postelem.append(elem('div').addClass('description').html(post.description));
                 scan.push(post.description);
             }
         }
@@ -163,12 +163,12 @@ function cb(data) {
         // Chat
         else if (post.type == 'chat') {
             if (post.title) {
-                postelem.append($(elem('h2')).addClass('title').text(post.title));
+                postelem.append(elem('h2').addClass('title').text(post.title));
                 scan.push(post.title);
             }
-            chat = $(elem('ul')).addClass('dialogue');
+            chat = elem('ul').addClass('dialogue');
             $.each(post.dialogue, function(l, line) {
-                chat.append($(elem('li')).addClass('line').text(
+                chat.append(elem('li').addClass('line').text(
                     line.label + " " + line.phrase
                 ));
                 scan.push(line.label);
@@ -179,35 +179,35 @@ function cb(data) {
 
         // Audio
         else if (post.type == 'audio') {
-            var audiobox = $(elem('div')).addClass('audiobox');
+            var audiobox = elem('div').addClass('audiobox');
             audiobox.append($(post.player).addClass('player'));
             if (post.album_art) {
                 audiobox.append(
-                    $(elem('img')).addClass('album_art').attr('src', post.album_art)
+                    elem('img').addClass('album_art').attr('src', post.album_art)
                 );
             }
             if (post.track_name) {
                 audiobox.append(
-                    $(elem('p')).addClass('track_name').text(post.track_name)
+                    elem('p').addClass('track_name').text(post.track_name)
                 );
                 scan.push(post.track_name);
             }
             if (post.artist) {
                 audiobox.append(
-                    $(elem('p')).addClass('artist').text(post.artist)
+                    elem('p').addClass('artist').text(post.artist)
                 );
                 scan.push(post.artist);
             }
             if (post.album) {
                 audiobox.append(
-                    $(elem('p')).addClass('album').text(post.album)
+                    elem('p').addClass('album').text(post.album)
                 );
                 scan.push(post.album);
             }
             postelem.append(audiobox);
             if (post.caption) {
                 postelem.append(
-                    $(elem('div')).addClass('caption').html(post.caption)
+                    elem('div').addClass('caption').html(post.caption)
                 );
                 scan.push(post.caption);
             }
@@ -220,7 +220,7 @@ function cb(data) {
             postelem.append($(best_player.embed_code).addClass('player'));
             if (post.caption) {
                 postelem.append(
-                    $(elem('div')).addClass('caption').html(post.caption)
+                    elem('div').addClass('caption').html(post.caption)
                 );
                 scan.push(post.caption);
             }
@@ -228,10 +228,10 @@ function cb(data) {
 
         // Answer
         else if (post.type == 'answer') {
-            var answerbox = $(elem('div')).addClass('answerbox');
-            answerbox.append($(elem('p')).addClass('question').text(post.question));
-            var asking = $(elem('p')).addClass('asking');
-            asking.append($(elem('img'))
+            var answerbox = elem('div').addClass('answerbox');
+            answerbox.append(elem('p').addClass('question').text(post.question));
+            var asking = elem('p').addClass('asking');
+            asking.append(elem('img')
                 .addClass('asking_avatar')
                 .attr('src', (post.asking_name == 'Anonymous') ?
                     ('http://assets.tumblr.com/images/anonymous_avatar_24.gif') :
@@ -240,7 +240,7 @@ function cb(data) {
                         + '.tumblr.com/avatar/24')
                 )
             );
-            asking_name = $(elem('a'))
+            asking_name = elem('a')
                 .addClass('asking_name')
                 .text(post.asking_name);
             if (post.asking_url) {
@@ -254,14 +254,14 @@ function cb(data) {
             asking.append(asking_name);
             answerbox.append(asking);
             postelem.append(answerbox);
-            postelem.append($(elem('div')).addClass('answer').html(post.answer));
+            postelem.append(elem('div').addClass('answer').html(post.answer));
             scan.push(post.answer);
 
         }
         
         // ???
         else {
-            postelem.append($(elem('i')).text(
+            postelem.append(elem('i').text(
                 "Sorry, I don't know how to render " + post.type + " posts yet."
             ));
         }
@@ -285,15 +285,15 @@ function cb(data) {
         });
         if (keywords.length && blacklist) {
             postelem.addClass('blacklisted');
-            var notification = $(elem('div')).addClass('notification');
+            var notification = elem('div').addClass('notification');
             notification.append(
-                $(elem('h2')).text('Post blacklisted')
+                elem('h2').text('Post blacklisted')
             );
             notification.append(
-                $(elem('p')).text('This post contains the keyword' +
+                elem('p').text('This post contains the keyword' +
                     plural(keywords) + ' ' + niceList(keywords) + '.')
             );
-            notification.append($(elem('a'))
+            notification.append(elem('a')
                 .addClass('instructions')
                 .text(
                     touchscreen ? 'Press and hold to unhide'
