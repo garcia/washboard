b1bcd4db9c258a8417ef0a809878d22024045ea5
Grant Garcia <github@grantgarcia.org>
2013-03-08 23:58:58 -0700

    Redirect favicon.ico

--- washboard/urls.py
+++ washboard/urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^dash', 'wb.views.dash.main'),
 
     url(r'^rules', 'wb.views.rules.main'),
+    
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 )
