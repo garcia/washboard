9cc0fdad28ba7645ac0868530441f30175dda562
Grant Garcia <github@grantgarcia.org>
2013-03-15 00:16:29 -0700

    Fixed undesired selection during unhiding of posts
    Also added lots of comments to dash.js + general cleanup.
    Too lazy to separate into 2 commits.

--- static/js/dash.js
+++ static/js/dash.js
@@ -42,41 +42,51 @@ function cb(data) {
         $('#load_more').text('There was an error while loading your posts. Try again?');
         $('#load_more').removeClass('loading');
         
+        // Stacktrace
         console.log(e.stack);
     }
 }
 function dash(data) {
-    //console.log(JSON.stringify(data));
+    // Convenience function to create new jQuery-wrapped elements
     this.elem = function(elem) {
         if(typeof(elem) != "string") {
             return false;
         }
         return $(document.createElement(elem));
     }
+
+    // Global variable for debugging purposes
     d = data;
+
+    // Build posts
     var posts_elem = elem('ul').attr('id', 'posts');
     $.each(data.response.posts, function(p, post) {
-        // Check post's ID
+        
+        // Check post's ID against currently loaded posts
         if (post.id >= $(posts).last().prop('id')) {
+            // If it wasn't posted before the last post, ignore it
+            // and note that we're now one more post behind
             behind_by++;
             return true;
         }
-        posts.push(post);
-
-        //console.log(post);
+        
+        // General setup
+        var scan = [];
         var postelem = elem('li').addClass('post');
         postelem.addClass(post.type);
         postelem.attr('id', 'post_' + post.id);
+        posts.push(post);
 
-        var scan = [];
-
-        // Common metadata
+        // Metadata
         var meta = elem('div').addClass('meta');
+
+        // Blog name
         meta.append(elem('a')
             .addClass('blog_name')
             .text(post.blog_name)
             .attr('href', post.post_url)
         );
+        // Reblogged blog name
         if (post.reblogged_from_name) {
             meta.append(elem('span')
                 .addClass('reblogged_text')
@@ -89,15 +99,22 @@ function dash(data) {
             );
             scan.push(post.reblogged_from_name);
         }
+
+        // Buttons
         buttons = elem('div').addClass('buttons');
+
+        // Note count
         if (post.note_count) {
             buttons.append(elem('a')
                 .addClass('note_count')
                 .text(post.note_count)
             );
         }
+
         // TODO: don't show like / reblog buttons if it's your own post
         if (true) {
+
+            // Like button
             var like_button = elem('a')
                 .addClass('like')
                 .text('Like')
@@ -111,6 +128,8 @@ function dash(data) {
                 like_button.addClass('liked');
             }
             buttons.append(like_button);
+
+            // Reblog button
             buttons.append(elem('a')
                 .addClass('reblog')
                 .text('Reblog')
@@ -125,30 +144,40 @@ function dash(data) {
         );
         meta.append(buttons);
         postelem.append(meta);
-
-        // Text
+        
+        // Text posts
         if (post.type == 'text') {
+
+            // Title (optional)
             if (post.title) {
                 postelem.append(elem('h2').text(post.title));
                 scan.push(post.title);
             }
+
+            // Body
             postelem.append(elem('div').addClass('body').html(post.body));
             scan.push(post.body);
         }
 
-        // Photo
+        // Photo posts
         else if (post.type == 'photo') {
             var photos = elem('div').addClass('photos');
             var row = elem('div').addClass('row');
             var last_row = 0;
+
+            // Insert each photo
             $.each(post.photos, function(ph, photo) {
-                // Photoset logic
+
+                // Get photoset layout
                 if (post.photoset_layout) {
                     layout = post.photoset_layout;
                 }
+                // Pseudo-layout for single images
                 else {
                     layout = '1';
                 }
+
+                // Get row of current photo
                 var running_total = 0;
                 var running_row = 0;
                 while (running_total <= ph) {
@@ -162,6 +191,8 @@ function dash(data) {
                     row = elem('div').addClass('row');
                 }
                 last_row = running_row;
+
+                // Determine optimal photo size to load
                 var target_size = {'1': 500 * scale, '2': 245 * scale, '3': 160 * scale}[
                     layout[running_row]
                 ];
@@ -173,46 +204,62 @@ function dash(data) {
             row.addClass('row-' + layout[last_row]);
             photos.append(row);
             postelem.append(photos);
+
+            // Caption (optional)
             if (post.caption) {
                 postelem.append(elem('div').addClass('caption').html(post.caption));
                 scan.push(post.caption);
             }
         }
 
-        // Quote
+        // Quote posts
         else if (post.type == 'quote') {
+            
+            // Quote
             postelem.append(elem('div').addClass('quote').html(post.text));
             scan.push(post.text);
+
+            // Source (optional)
             if (post.source) {
                 postelem.append(elem('div').addClass('source').html(post.source));
                 scan.push(post.source);
             }
         }
 
-        // Link
+        // Link posts
         else if (post.type == 'link') {
+            
+            // URL
             var anchor = elem('a').addClass('link').attr('href', post.url);
             if (post.title) {
+                // Title, if present
                 anchor.text(post.title);
             }
             else {
+                // Title defaults to the URL itself
                 anchor.text(post.url);
             }
             scan.push(post.title);
             scan.push(post.url);
             postelem.append(anchor);
+
+            // Description (optional)
             if (post.description) {
                 postelem.append(elem('div').addClass('description').html(post.description));
                 scan.push(post.description);
             }
         }
 
-        // Chat
+        // Chat posts
         else if (post.type == 'chat') {
+            
+            // Title (optional)
             if (post.title) {
                 postelem.append(elem('h2').addClass('title').text(post.title));
                 scan.push(post.title);
             }
+
+            // Dialogue
             chat = elem('ul').addClass('dialogue');
             $.each(post.dialogue, function(l, line) {
                 chat.append(elem('li').addClass('line').text(
@@ -224,9 +271,13 @@ function dash(data) {
             postelem.append(chat);
         }
 
-        // Audio
+        // Audio posts
         else if (post.type == 'audio') {
+
+            // Audio box (container for non-caption data)
             var audiobox = elem('div').addClass('audiobox');
+
+            // HTML5 player, if possible
             if (post.audio_url) {
                 // Apparently this is required, otherwise it gives a 403...
                 if (post.audio_url.indexOf('http://www.tumblr.com/') == 0) {
@@ -240,9 +291,12 @@ function dash(data) {
                         .addClass('new')
                 );
             }
+            // Default to embedded player
             else {
                 audiobox.append($(post.player).addClass('player'));
             }
+
+            // Album art (optional)
             if (post.album_art) {
                 var album_art = elem('img')
                     .addClass('album_art')
@@ -250,6 +304,8 @@ function dash(data) {
                     .click(function() { $(this).toggleClass('expanded'); });
                 audiobox.append(album_art);
             }
+
+            // Track name, artist, and album (all optional)
             var metadata = ['track_name', 'artist', 'album'];
             for (var i = 0; i < metadata.length; i++) {
                 if (post[metadata[i]]) {
@@ -259,6 +315,8 @@ function dash(data) {
                 }
             }
             postelem.append(audiobox);
+
+            // Caption (optional)
             if (post.caption) {
                 postelem.append(
                     elem('div').addClass('caption').html(post.caption)
@@ -267,11 +325,14 @@ function dash(data) {
             }
         }
 
-        // Video
+        // Video posts
         else if (post.type == 'video') {
-            // TODO: choose video size intelligently
+            
+            // Player
             var best_player = best_fit(post.player, 500 * scale);
             postelem.append($(best_player.embed_code).addClass('player'));
+
+            // Caption (optional)
             if (post.caption) {
                 postelem.append(
                     elem('div').addClass('caption').html(post.caption)
@@ -280,11 +341,18 @@ function dash(data) {
             }
         }
 
-        // Answer
+        // Answer posts
         else if (post.type == 'answer') {
+            
+            // Answer box
             var answerbox = elem('div').addClass('answerbox');
+
+            // Question
             answerbox.append(elem('p').addClass('question').text(post.question));
+
+            // Asker's information
             var asking = elem('p').addClass('asking');
+            // Avatar
             asking.append(elem('img')
                 .addClass('asking_avatar')
                 .attr('src', (post.asking_name == 'Anonymous') ?
@@ -294,9 +362,11 @@ function dash(data) {
                         + '.tumblr.com/avatar/24')
                 )
             );
+            // Name
             asking_name = elem('a')
                 .addClass('asking_name')
                 .text(post.asking_name);
+            // Blog URL
             if (post.asking_url) {
                 asking_name.attr('href', post.asking_url);
             }
@@ -308,12 +378,14 @@ function dash(data) {
             asking.append(asking_name);
             answerbox.append(asking);
             postelem.append(answerbox);
+
+            // Answer
             postelem.append(elem('div').addClass('answer').html(post.answer));
             scan.push(post.answer);
 
         }
         
-        // ???
+        // Unidentifiable posts
         else {
             postelem.append(elem('i').text(
                 "Sorry, I don't know how to render " + post.type + " posts yet."
@@ -324,31 +396,49 @@ function dash(data) {
         keywords = [];
         blacklist = true;
         notification = true;
+
+        // Iterate over elements to be scanned
         $.each(scan, function(s, scan_element) {
+            
+            // Iterate over the user's rules
             $.each(rules, function(r, rule) {
                 var kw = rule.keyword;
+
+                // Convert "whole words" to Regex patterns
+                // TODO: this will cause unexpected results if the word contains
+                //       valid Regex code!
                 if (rule.whole_word) {
                     kw = new RegExp('\\b' + kw + '\\b', 'i');
                     rule.regex = true;
                 }
+
                 if ((rule.regex && scan_element.search(kw) >= 0) ||
                     (!rule.regex && scan_element.toLowerCase().indexOf(kw.toLowerCase()) >= 0)) {
-                    // Post contains a whitelisted keyword
+                    
+                    // Post contains a whitelisted keyword; stop scanning
                     if (!rule.blacklist) {
                         blacklist = false;
                         return false;
                     }
+
+                    // Don't give the user the option to show the post
                     if (!rule.show_notification) {
                         notification = false;
                     }
+                    
+                    // Note that the keyword has already been found
                     if (keywords.indexOf(rule.keyword) == -1) {
                         keywords.push(rule.keyword);
                     }
                 }
             });
         });
+
+        // Mark the post as blacklisted if necessary
         if (keywords.length && blacklist && notification) {
             postelem.addClass('blacklisted');
+
+            // Insert notification
             var notification = elem('div').addClass('notification');
             notification.append(
                 elem('h2').text('Post blacklisted')
@@ -364,23 +454,30 @@ function dash(data) {
                                 : 'Click to unhide')
             );
             notification.append(elem('div').addClass('progress'));
+
+            // Add listeners for touchscreens
             if (touchscreen) {
-                console.log('hmm');
                 postelem.on('touchstart', touchstart);
                 postelem.on('touchend', touchend);
             }
+            // Default to a simple click listener
             else {
                 notification.attr('onclick', 'unhide(this)');
             }
+
             postelem.append(notification);
         }
 
         // Check for read-more breaks
         postelem.find('p').contents().filter(function() {
+            // Select comment nodes
             return this.nodeType == 8;
         }).each(function(i, e) {
+            
+            // Check contents of comment
             if (e.nodeValue == ' more ') {
-                // Insert "Read more" link
+
+                // Replace comment with "Read more" link
                 var more_link = elem('a')
                         .html('Read more &rarr;')
                         .addClass('read_more js')
@@ -389,6 +486,7 @@ function dash(data) {
                             $(this).remove();
                         });
                 $(e).replaceWith(more_link);
+
                 // Hide anything that appears after the comment but within
                 // the same paragraph by iterating over sibling nodes
                 var parent_node = more_link.get(0).parentNode;
@@ -401,25 +499,28 @@ function dash(data) {
                     parent_node.replaceChild(span, sibling);
                     sibling = sibling.nextSibling;
                 }
+
                 // Hide everything else
                 more_link.parent().nextAll().addClass('cut under')
             }
         });
         
-
         // Append element, unless it got blacklisted by a no-notification keyword
         if (!keywords.length || notification) {
             posts_elem.append(postelem);
         }
     });
+
+    // Insert all posts
     $('#middle > div > #posts').append(posts_elem.children());
 
+    // Reset "Load more" footer
     if ($('#load_more').hasClass('loading')) {
         $('#load_more').text('Load more');
         $('#load_more').removeClass('loading');
     }
 
-    // Convert audio to MediaElement
+    // Convert new audio elements to MediaElement players
     $('audio.new').mediaelementplayer({
         audioWidth: '100%',
         audioHeight: 30,
@@ -435,7 +536,13 @@ function dash(data) {
 }
 
 function touchstart(e) {
-    $('body').css('-webkit-user-select', 'none');
+    // Don't re-allow selection if we've already received another tap
+    clearTimeout(allow_selection);
+    
+    // Disallow selection
+    $('body').attr('style', '-webkit-user-select: none; -webkit-touch-callout: none');
+
+    // Unhide after 1 second
     unhiding = setTimeout(function() {
         var prog = $(e.currentTarget).find('.progress');
         prog.stop()
@@ -445,18 +552,23 @@ function touchstart(e) {
                 1000,
                 function() {
                     unhide(e.currentTarget);
-                    $('body').css('-webkit-user-select', 'text');
+                    // Re-allow selection
+                    $('body').removeAttr('style');
+                    // Remove touch handlers
                     $(e.currentTarget).off('touchstart').off('touchend');
                 }
             );
     }, 50);
-    //checktouch = setInterval(function() {
-        
 }
 
 function touchend(e) {
-    $('body').css('-webkit-user-select', 'text');
-    console.log(e);
+    // Re-allow selection after 300ms; without the delay,
+    // iOS will try to select the progress slider immediately upon touchend
+    allow_selection = setTimeout(function() {
+        $('body').removeAttr('style');
+    }, 300);
+
+    // Stop unhiding the post
     clearTimeout(unhiding);
     var prog = $(e.currentTarget).find('.progress');
     prog.stop()
@@ -471,10 +583,13 @@ function touchend(e) {
 
 function unhide(a) {
     var post = $(a).closest('.post');
+
+    // Fade out notification to white
     post.children().animate(
         {opacity: 0},
         500,
         function() {
+            // Fade in post from white
             post.removeClass('blacklisted');
             post.children().css('opacity', 0).animate({opacity: 1}, 600);
         }
@@ -483,6 +598,7 @@ function unhide(a) {
 
 function apicall(url, data, options) {
     var _data = $.extend({
+        // Empty body hash (perhaps I shouldn't assume it'll always be empty?)
         oauth_body_hash: '2jmj7l5rSw0yVb/vlWAYkK/YBwk='
     }, data);
     var _options = $.extend({
@@ -490,6 +606,7 @@ function apicall(url, data, options) {
         data: _data,
         dataType: 'jsonp',
         jsonp: false,
+        // Allow caching, otherwise jQuery messes with the query string
         cache: true,
         consumerKey: API_KEY,
         consumerSecret: API_SECRET,
@@ -513,19 +630,25 @@ function dashboard(data, options) {
 function like(data, options) {
     var cb = 'liked_' + data.id;
     var id = data.id;
+    
+    // Create global callback function for this post
     window[cb] = function(data) {
         if (data.meta.status == 200) {
             $('#post_' + id).find('.like').toggleClass('liked');
         }
     }
+
+    // Determine whether to like or unlike the post
+    mode = $('#post_' + id).find('.like').hasClass('liked') ? 'unlike' : 'like';
+
     var _data = $.extend({
         callback: cb,
-    }, data);
-    mode = $('#post_' + id).find('.like').hasClass('liked') ? 'unlike' : 'like';
+    }, data); 
     apicall('http://api.tumblr.com/v2/user/' + mode, _data, options);
 }
 
 function load_more() {
+    // Set the offset, accounting for posts that have been made since initial load
     dashboard({
         offset: $('#posts').children().length + behind_by * 2
     });
@@ -534,12 +657,26 @@ function load_more() {
 }
 
 $(function() {
+    // Get scale for photos
+    // TODO: adjust when window resizes?
     scale = Math.min(500, screen.width) / 500;
+    
+    // Detect touch screen
     touchscreen = 'ontouchstart' in window;
+    
+    // Remove unnecessary elements from center
     $('#middle > div').html('<div id="posts"></div>');
+    
+    // Initial variables
     posts = [];
     behind_by = 0;
+    allow_selection = -1;
+    unhiding = -1;
+    
+    // Load first page of dashboard
     dashboard();
+
+    // Insert new post button
     $('#new').addClass('dropdown').addClass('dropdown-tip');
     $('#new ul').addClass('dropdown-menu');
     $('#new').before('<a class="js" id="new-link" data-dropdown="#new">New</a>');;
