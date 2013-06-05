5b51d39cde2ef0d476d541462c4e0ccd316abeea
Grant Garcia <github@grantgarcia.org>
2013-06-04 18:15:39 -0700

    Removed unnecessary login / namechange views

--- washboard/urls.py
+++ washboard/urls.py
@@ -3,7 +3,6 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     url(r'^$', 'wb.views.welcome.main'),
-    url(r'^login$', 'wb.views.login.login'),
     url(r'^logout$', 'wb.views.login.logout'),
     url(r'^register$', 'wb.views.register.main'),
     url(r'^callback$', 'wb.views.register.callback'),
@@ -15,7 +14,6 @@ urlpatterns = patterns('',
     url(r'^rules$', 'wb.views.rules.main'),
     url(r'^hide$', 'wb.views.rules.hide'),
     url(r'^settings$', 'wb.views.settings.main'),
-    url(r'^namechange$', 'wb.views.namechange.main'),
     url(r'^faq$', 'wb.views.faq.main'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 )
