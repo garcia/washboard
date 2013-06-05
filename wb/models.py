4ad45e0898e18dd70388fe3f5b2015b04a922304
Grant Garcia <github@grantgarcia.org>
2013-06-04 18:27:05 -0700

    Removed all unnecessary models

--- wb/models.py
+++ wb/models.py
@@ -23,24 +23,12 @@ add_introspection_rules([], ['^wb\.models\.LowerCaseCharField'])
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    api_key = models.TextField(max_length=64)
-    api_secret = models.TextField(max_length=64)
-    token_key = models.TextField(max_length=64)
-    token_secret = models.TextField(max_length=64)
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         profile, created = UserProfile.objects.get_or_create(user=instance)
 
 models.signals.post_save.connect(create_user_profile, sender=User)
-
-
-class TemporaryKeypair(models.Model):
-    api_key = models.TextField(max_length=64)
-    api_secret = models.TextField(max_length=64)
-    token_key = models.TextField(max_length=64)
-    token_secret = models.TextField(max_length=64)
-    nonce = models.TextField(max_length=64)
 
 
 class Rule(models.Model):
