c847504b35d21d160bf4f84b9d849129e4552e62
Grant Garcia <github@grantgarcia.org>
2013-06-20 00:51:08 -0700

    Expose profile on dash-like pages
    Also, don't expose user / object IDs in the Washboard object

--- wb/views/dash.py
+++ wb/views/dash.py
@@ -6,6 +6,7 @@ import urlparse
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import RequestContext
@@ -31,17 +32,22 @@ def main(request, data_=None):
         'dash': True,
         'wb': {
             'username': request.user.username,
+            'profile': model_to_dict(request.user.get_profile(), exclude=('id', 'user')),
             'blogs': request.session.get('blogs', []),
             'parameters': {},
             'base_url': settings.BASE_URL,
             'endpoint': 'dashboard',
             'well_ordered': True,
-            'rules': list(
-                Rule.objects.filter(user__exact=request.user).values()
-            ),
-            'hidden_posts': list(
-                HiddenPost.objects.filter(user__exact=request.user).values()
-            ),
+            'rules': [
+                {field: rule[field] for field in rule
+                    if field not in ('user_id', 'id', 'index')}
+                for rule in Rule.objects.filter(user__exact=request.user).values()
+            ],
+            'hidden_posts': [
+                {field: hp[field] for field in hp
+                    if field not in ('user_id', 'id', 'index')}
+                for hp in HiddenPost.objects.filter(user__exact=request.user).values()
+            ],
         },
     }
     if data_: update(data, data_)
